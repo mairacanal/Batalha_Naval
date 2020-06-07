feca099dc22c9c652d536360fc778da57661b46b
Caroline Grupe <63861400+Carolgrupe@users.noreply.github.com>
2020-06-07 09:39:53 -0300

    Update constants.h

--- VERSÃO FINAL/constants.h
+++ VERSÃO FINAL/constants.h
@@ -1,14 +1,14 @@
-#define LINHAS 17
-#define COLUNAS 17
-#define STRINGS 289
-#define NUM_CHAR 7
+#define LINHAS 17                     // Número de linhas do tabuleiro "back-end"
+#define COLUNAS 17                    // Número de colunas do tabuleiro "back-end"
+#define STRINGS 289                   // Número de strings do vetor que forma o tabuleiro "front-end" 
+#define NUM_CHAR 7                    // Número máximo de caracteres de cada uma das strings do vetor
 
-#define PONTUA_P 43
-#define PONTUA_C 78
-#define PONTUA_T 82
-#define PONTUA_H 125
-#define PONTUA_ERRO -3
+#define PONTUA_P 43                   // Pontuação recebida quando o usuário atinge um elemento do Porta-aviões
+#define PONTUA_C 78                   // Pontuação recebida quando o usuário atinge um elemento do Couraçado
+#define PONTUA_T 82                   // Pontuação recebida quando o usuário atinge um elemento do Torpedeiro
+#define PONTUA_H 125                  // Pontuação recebida quando o usuário atinge um elemento do Hidro-avião
+#define PONTUA_ERRO -3                // Pontuação perdida quando o usuário atinge a água
 
-#define NUM_COURACADOS 2               // Número de couraçados (2)
-#define NUM_TORPEDEIROS 3              // Número de torpedeiros (3)
-#define NUM_HIDROAVIOES 4              // Número de hidroaviões (4)
+#define NUM_COURACADOS 2              // Número de couraçados (2)
+#define NUM_TORPEDEIROS 3             // Número de torpedeiros (3)
+#define NUM_HIDROAVIOES 4             // Número de hidroaviões (4)
